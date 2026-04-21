2c8eaf885557ecac49ce23f0435e9fbc0bdb2136
therodma <roderickma2015@gmail.com>
2026-04-21 13:01:33 -0700

    Switch to HashRouter for GitHub Pages compatibility

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -14,6 +14,6 @@ export default function App() {
       <footer>
         <p>© {new Date().getFullYear()} Roderick Ma · UC Merced</p>
       </footer>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
